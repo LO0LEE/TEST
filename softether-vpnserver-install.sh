e79575614c606d205b730839d618653156ad070b
LO0LEE <124404136+LO0LEE@users.noreply.github.com>
2024-03-15 02:35:44 +0630

    softether-vpnserver-install.sh

--- softether-vpnserver-install.sh
+++ softether-vpnserver-install.sh
@@ -40,13 +40,13 @@ chmod 700 vpncmd
 chmod 700 vpnserver
 
 #grab Softether vpn server.config template
-#wget -O /usr/local/vpnserver/vpn_server.config https://github.com/LO0LEE/TEST/vpn_server.config
+wget -O /usr/local/vpnserver/vpn_server.config https://raw.githubusercontent.com/LO0LEE/TEST/vpn_server.config
 
 #Create systemd init file for Softether VPN service
-wget -O /lib/systemd/system/vpnserver.service https://github.com/LO0LEE/TEST/vpnserver.service
+wget -O /lib/systemd/system/vpnserver.service https://raw.githubusercontent.com/LO0LEE/TEST/vpnserver.service
 #Grab DNSMasq conf
-wget -O /etc/dnsmasq.conf https://github.com/LO0LEE/TEST/dnsmasq.conf
-wget -O /etc/logrotate.d/dnsmasq https://github.com/LO0LEE/TESTdnsmasq
+wget -O /etc/dnsmasq.conf https://raw.githubusercontent.com/LO0LEE/TEST/dnsmasq.conf
+wget -O /etc/logrotate.d/dnsmasq https://raw.githubusercontent.com/LO0LEE/TEST/dnsmasq
 
 shopt -s extglob; NET_INTERFACE=$(ip link | awk -F: '$0 !~ "lo|vir|wl|tap_soft|^[^0-9]"{print $2;getline}'); NET_INTERFACE="${NET_INTERFACE##*( )}"; sed -i s/ens3/"$NET_INTERFACE"/g /etc/dnsmasq.conf; shopt -u extglob;
 
@@ -59,7 +59,7 @@ shopt -s extglob; NET_INTERFACE=$(ip link | awk -F: '$0 !~ "lo|vir|wl|tap_soft|^
 #cat <(fgrep -i -v "$command" <(crontab -l)) <(echo "$job") | crontab -
 
 #Grab needed Log purging 
-#wget -O /root/softetherlogpurge.sh https://raw.githubusercontent.com/hazemhak/softethernat/master/softetherlogpurge.sh; chmod a+x /root/softetherlogpurge.sh;
+#wget -O /root/softetherlogpurge.sh https://raw.githubusercontent.com/LO0LEE/TEST/softetherlogpurge.sh; chmod a+x /root/softetherlogpurge.sh;
 
 #Install Log purging.
 #command2="/root/softetherlogpurge.sh >/dev/null 2>&1"
@@ -77,11 +77,11 @@ wget -O /root/softether-iptables.sh https://github.com/LO0LEE/TEST/softether-ipt
 #systemctl enable monit
 #systemctl start monit
 #systemctl status monit
-#wget -O /etc/monit/monitrc https://raw.githubusercontent.com/hazemhak/softethernat/master/monit/monitrc
-#wget -O /etc/monit/conf-enabled/openssh-server https://raw.githubusercontent.com/hazemhak/softethernat/master/monit/openssh-server
-#wget -O /etc/monit/conf-enabled/cron https://raw.githubusercontent.com/hazemhak/softethernat/master/monit/cron
-#wget -O /etc/monit/conf-enabled/dnsmasq https://raw.githubusercontent.com/hazemhak/softethernat/master/monit/dnsmasq
-#wget -O /etc/monit/conf-enabled/vpnserver https://raw.githubusercontent.com/hazemhak/softethernat/master/monit/vpnserver
+#wget -O /etc/monit/monitrc https://raw.githubusercontent.com/LO0LEE/TEST/monit/monitrc
+#wget -O /etc/monit/conf-enabled/openssh-server https://raw.githubusercontent.com/LO0LEE/TEST/monit/openssh-server
+#wget -O /etc/monit/conf-enabled/cron https://raw.githubusercontent.com/LO0LEE/TEST/monit/cron
+#wget -O /etc/monit/conf-enabled/dnsmasq https://raw.githubusercontent.com/LO0LEE/TEST/monit/dnsmasq
+#wget -O /etc/monit/conf-enabled/vpnserver https://raw.githubusercontent.com/LO0LEE/TEST/monit/vpnserver
 #systemctl restart monit
 
 # install webmin
